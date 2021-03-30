2254bfed4eeefbfea2c5f895cabff91304a86bc7
joejnke <joejnek@gmail.com>
2021-03-30 12:44:43 +0300

    Auto increment changeID of Change table and Add triggers

--- changelogSchema.sql
+++ changelogSchema.sql
@@ -1,14 +1,82 @@
 -- Create Tables
 CREATE TABLE Change (
-	ChangeID INTEGER PRIMARY KEY,
-	HelpDeskRequestID INTEGER,
-	ChangeStatus VARCHAR DEFAULT 'Change logged',
-	Remark VARCHAR,
+	ChangeID INT IDENTITY(1,1) PRIMARY KEY,
+	HelpDeskRequestID INT,
+	ChangeStatus VARCHAR(max) DEFAULT 'Change logged',
+	Remark VARCHAR(max),
 	Environment VARCHAR(100),
-	ChangeType VARCHAR(50),
+	ChangeType VARCHAR(100),
 	CONSTRAINT check_changetype CHECK (ChangeType IN ('Emergency', 'Pre Approved', 'Standard')),
 	CONSTRAINT check_changestatus CHECK (ChangeStatus IN ('Change logged', 'Problem Identified', 'Change Requested',
 													  'Change Planned', 'Change Approved', 'Change Applied'))
+	)
+
+CREATE TABLE Problem (
+	ChangeID INT PRIMARY KEY FOREIGN KEY REFERENCES Change(ChangeID),
+	Description VARCHAR(max),
+	IdentifiedDate DATE,
+	IdentifiedBy VARCHAR(100),
+	Remark VARCHAR(max)
+	)
+
+CREATE TABLE ChangeRequest (
+	ChangeID INT PRIMARY KEY FOREIGN KEY REFERENCES Problem(ChangeID),
+	Description VARCHAR(max),
+	RequestedDate DATE,
+	RequestedBY VARCHAR(100),
+	ApprovedBY VARCHAR(100),
+	ApprovedDate DATE,
+	Department VARCHAR(100),
+	AffectedModules VARCHAR(100),
+	AffectedDepartments VARCHAR(100),
+	Remark VARCHAR(max)
+	)
+	
+CREATE TABLE ChangePlan (
+	ChangeID INT PRIMARY KEY FOREIGN KEY REFERENCES ChangeRequest(ChangeID),
+	Description VARCHAR(max),
+	PlannedDate DATE,
+	PlannedBY VARCHAR(100),
+	TargetSoftware VARCHAR(100),
+	TargetModule VARCHAR(100),
+	AffectedModules VARCHAR(100),
+	AffectedDepartments VARCHAR(100),
+	Remark VARCHAR(max)
+	)
+
+CREATE TABLE ApprovedChange (
+	ChangeID INT PRIMARY KEY FOREIGN KEY REFERENCES ChangePlan(ChangeID),
+	Description VARCHAR(max),
+	ApprovedBY VARCHAR(100),
+	ApprovedDate DATE,
+	Remark VARCHAR(max)
+	)
+
+CREATE TABLE AppliedChange (
+	ChangeID INT PRIMARY KEY FOREIGN KEY REFERENCES ApprovedChange(ChangeID),
+	Description VARCHAR(max),
+	AppliedBY VARCHAR(100),
+	AppliedDate DATE,
+	Remark VARCHAR(max)
+	)
+
+CREATE TABLE ChangeHistory (
+	ChangeID INT PRIMARY KEY FOREIGN KEY REFERENCES Change(ChangeID),
+	IncidentReportDate DATE,
+	ProblemIdentifiedDate DATE,
+	ChangeRequestedDate DATE,
+	ChangePlannedDate DATE,
+	ChangeApprovedDate DATE,
+	ChangeAppliedDate DATE,
+	Remark VARCHAR(max)
+	)
+
+CREATE TABLE Reference (
+	ChangeID INT FOREIGN KEY REFERENCES Change(ChangeID),
+	RefType VARCHAR(100),
+	RefID VARCHAR(100),
+	PRIMARY KEY (ChangeID, RefType, RefID),
+	Remark VARCHAR(max)
 	)
 
 -- Update history on Change logged
@@ -23,84 +91,91 @@ BEGIN
 	SELECT ChangeID, getdate()
 	FROM inserted
 END
+-- end of trigger
 
-CREATE TABLE Problem (
-	ChangeID INTEGER PRIMARY KEY FOREIGN KEY REFERENCES Change(ChangeID),
-	Description VARCHAR,
-	IdentifiedDate DATE,
-	IdentifiedBy VARCHAR(100),
-	Remark VARCHAR
-	)
+-- update change log history and status at every stage of the change flow
 
--- update change log history and status
-CREATE TRIGGER UPDATE_ChangeStatus_N_ChangeHistory ON Problem
+-- trigger on Problem Identification stage
+CREATE TRIGGER UPDATE_Status_N_History_ON_Problem ON Problem
 FOR INSERT 
 NOT FOR REPLICATION 
 AS
  
 BEGIN
-  INSERT INTO ChangeHistory 
-	(ChangeID, ProblemIdentifiedDate)
-	SELECT ChangeID, getdate()
-	FROM inserted
+  UPDATE ChangeHistory 
+	SET	ProblemIdentifiedDate = (SELECT IdentifiedDate FROM INSERTED)
+	WHERE ChangeHistory.ChangeID = (SELECT ChangeID FROM INSERTED)
+
+  UPDATE Change
+	SET ChangeStatus='Problem Identified'
+	WHERE Change.ChangeID = (SELECT ChangeID FROM INSERTED)
 END
+-- end of trigger
 
-CREATE TABLE ChangeRequest (
-	ChangeID INTEGER PRIMARY KEY FOREIGN KEY REFERENCES Problem(ChangeID),
-	Description VARCHAR,
-	RequestedDate DATE,
-	RequestedBY VARCHAR(100),
-	ApprovedBY VARCHAR(100),
-	ApprovedDate DATE,
-	Department VARCHAR(100),
-	AffectedModules VARCHAR(100),
-	AffectedDepartments VARCHAR(100),
-	Remark VARCHAR
-	)
-	
-CREATE TABLE ChangePlan (
-	ChangeID INTEGER PRIMARY KEY FOREIGN KEY REFERENCES ChangeRequest(ChangeID),
-	Description VARCHAR,
-	PlannedDate DATE,
-	PlannedBY VARCHAR(100),
-	TargetSoftware VARCHAR(100),
-	TargetModule VARCHAR(100),
-	AffectedModules VARCHAR(100),
-	AffectedDepartments VARCHAR(100),
-	Remark VARCHAR
-	)
+-- trigger on Change Requested stage
+CREATE TRIGGER UPDATE_Status_N_History_ON_ChangeRequest ON ChangeRequest
+FOR INSERT 
+NOT FOR REPLICATION 
+AS
+ 
+BEGIN
+  UPDATE ChangeHistory 
+	SET	ChangeRequestedDate = (SELECT RequestedDate FROM INSERTED)
+	WHERE ChangeHistory.ChangeID = (SELECT ChangeID FROM INSERTED)
 
-CREATE TABLE ApprovedChange (
-	ChangeID INTEGER PRIMARY KEY FOREIGN KEY REFERENCES ChangePlan(ChangeID),
-	Description VARCHAR,
-	ApprovedBY VARCHAR(100),
-	ApprovedDate VARCHAR(100),
-	Remark VARCHAR
-	)
+  UPDATE Change
+	SET ChangeStatus='Change Requested'
+	WHERE Change.ChangeID = (SELECT ChangeID FROM INSERTED)
+END
+-- end of trigger
 
-CREATE TABLE AppliedChange (
-	ChangeID INTEGER PRIMARY KEY FOREIGN KEY REFERENCES ApprovedChange(ChangeID),
-	Description VARCHAR,
-	AppliedBY VARCHAR(100),
-	AppliedDate DATE,
-	Remark VARCHAR
-	)
+-- trigger on Change Planned stage
+CREATE TRIGGER UPDATE_Status_N_History_ON_ChangePlan ON ChangePlan
+FOR INSERT 
+NOT FOR REPLICATION 
+AS
+ 
+BEGIN
+  UPDATE ChangeHistory 
+	SET	ChangePlannedDate = (SELECT PlannedDate FROM INSERTED)
+	WHERE ChangeHistory.ChangeID = (SELECT ChangeID FROM INSERTED)
 
-CREATE TABLE ChangeHistory (
-	ChangeID INTEGER PRIMARY KEY FOREIGN KEY REFERENCES Change(ChangeID),
-	IncidentReportDate DATE,
-	ProblemIdentifiedDate DATE,
-	ChangeRequestedDate DATE,
-	ChangePlannedDate DATE,
-	ChangeApprovedDate DATE,
-	ChangeAppliedDate DATE,
-	Remark VARCHAR
-	)
+  UPDATE Change
+	SET ChangeStatus='Change Planned'
+	WHERE Change.ChangeID = (SELECT ChangeID FROM INSERTED)
+END
+-- end of trigger
 
-CREATE TABLE Reference (
-	ChangeID INTEGER FOREIGN KEY REFERENCES Change(ChangeID),
-	RefType VARCHAR(100),
-	RefID VARCHAR(100),
-	PRIMARY KEY (ChangeID, RefType, RefID),
-	Remark VARCHAR
-	)
+-- trigger on Approved Change stage
+CREATE TRIGGER UPDATE_Status_N_History_ON_ApprovedChange ON ApprovedChange
+FOR INSERT 
+NOT FOR REPLICATION 
+AS
+ 
+BEGIN
+  UPDATE ChangeHistory 
+	SET	ChangeApprovedDate = (SELECT ApprovedDate FROM INSERTED)
+	WHERE ChangeHistory.ChangeID = (SELECT ChangeID FROM INSERTED)
+
+  UPDATE Change
+	SET ChangeStatus='Change Approved'
+	WHERE Change.ChangeID = (SELECT ChangeID FROM INSERTED)
+END
+-- end of trigger
+
+-- trigger on Applied Change stage
+CREATE TRIGGER UPDATE_Status_N_History_ON_AppliedChange ON AppliedChange
+FOR INSERT 
+NOT FOR REPLICATION 
+AS
+ 
+BEGIN
+  UPDATE ChangeHistory 
+	SET	ChangeAppliedDate = (SELECT AppliedDate FROM INSERTED)
+	WHERE ChangeHistory.ChangeID = (SELECT ChangeID FROM INSERTED)
+
+  UPDATE Change
+	SET ChangeStatus='Change Applied'
+	WHERE Change.ChangeID = (SELECT ChangeID FROM INSERTED)
+END
+-- end of trigger
